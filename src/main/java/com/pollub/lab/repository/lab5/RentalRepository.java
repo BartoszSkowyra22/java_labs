3c5a13b27ab2840aacb4d384cd8f24f0c8878321
BartoszSkowyra22 <skowyrab@gmail.com>
2024-12-02 22:43:31 +0100

    feat/integration_tests Add integration tests for Lab5

--- src/main/java/com/pollub/lab/repository/lab5/RentalRepository.java
+++ src/main/java/com/pollub/lab/repository/lab5/RentalRepository.java
@@ -1,17 +1,10 @@
 package com.pollub.lab.repository.lab5;
 
 import com.pollub.lab.model.lab5.Rental;
-import com.pollub.lab.model.lab5.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface RentalRepository extends JpaRepository<Rental, Long> {
-
-    List<Rental> findByCustomer(Customer customer);
-
-    List<Rental> findByVehicleType_Type(String type);
 }
 
